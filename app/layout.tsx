69b2bbb202a7dd5d6562207fd4c1b37f5a87b00e
Stellan Bergström <stellan.b@vagabond.com>
2024-11-04 14:37:22 +0100

    Links 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
